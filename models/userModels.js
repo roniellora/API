efe8e4e2046506d4a2500225b8447eca9caf3549
Roniel <roniellora8@gmail.com>
2024-06-07 23:17:09 +0800

    inayos lang sa users

--- models/userModels.js
+++ models/userModels.js
@@ -1,21 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please provide a name']
-    },
-    email: {
-        type: String,
-        required: [true, 'Please provide an email'],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, 'Please provide a password']
-    }
-})
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isEmployee: {
+    type: Boolean,
+    default: false,
+  },
+  notifications: {
+    type: Array,
+    default: [],
+  },
+  seenNotification: {
+    type: Array,
+    default: [],
+  },
+});
 
-const userModel = mongoose.model('users', userSchema)
+const userModel = mongoose.model("users", userSchema);
 
-module.exports = userModel
+module.exports = userModel;
